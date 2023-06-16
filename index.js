a41fbb9ac0517aa31149cb3cd385c7261bef6b52
karanja03 <wambuik03@gmail.com>
2023-06-16 15:49:45 +0300

    completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+ window.customerName = 'bob';
+  var bestCustomer;
+  const leastFavoriteCustomer = "Njoroge";
+
+
+ function upperCaseCustomerName(){
+     window.customerName = window.customerName.toUpperCase();
+ }
+ console.log(window.customerName)
+
+ 
+ function setBestCustomer(){
+    bestCustomer = 'not bob'
+  
+ }
+ console.log(setBestCustomer)
+
+ function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+
+ }
+ console.log(overwriteBestCustomer)
+
+ function changeLeastFavoriteCustomer(){ 
+    leastFavoriteCustomer = 'Mike';
+    return 'Assignment to constant variable.'
+ }
+ console.log(changeLeastFavoriteCustomer)
